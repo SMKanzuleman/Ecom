79f7d27eefccea2fc892a41fc36c025b0b9b84cb
SMKanzuleman <syedkanzuleman.official@gmail.com>
2026-07-24 00:19:43 +0500

    imp remove cart api

--- BackEnd/src/cart/cart.controller.ts
+++ BackEnd/src/cart/cart.controller.ts
@@ -53,7 +53,7 @@ export const AddToCart = async (req: AuthRequest, res: Response) => {
         }
         FoundedCart.CartPrice += FounedProduct.Price * Quantity
         await FoundedCart.save()
-        return SendSuccess(res, 200, `${FounedProduct.Name} added to cart😊`, {FoundedCart})
+        return SendSuccess(res, 200, `${FounedProduct.Name} added to cart😊`, { FoundedCart })
 
     } catch (error) {
         return SendError(res, 500, "error in adding product to cart")
@@ -66,13 +66,64 @@ export const GetCart = async (req: AuthRequest, res: Response) => {
     try {
         let { id } = req.User
         const FoundedCart = await Cart.findOne({ UserId: id }).populate("Items.ProductId")
-        if (!FoundedCart) { 
-            return SendError(res,404,"Cart Empty")
+        if (!FoundedCart) {
+            return SendError(res, 404, "Cart Empty")
         }
-        return SendSuccess(res, 200, "Cart items", {FoundedCart})
-        
+        return SendSuccess(res, 200, "Cart items", { FoundedCart })
+
     } catch (error) {
-        return SendError(res,500,"Unkonwn error")
+        return SendError(res, 500, "Unkonwn error")
     }
-    
+
+}
+
+export const RemoveFromCart = async (req: AuthRequest, res: Response) => {
+    try {
+        let { id: userId } = req.User
+        let { id: productId } = req.params
+        const FoundedProduct = await Product.findById(productId)
+        if (!FoundedProduct) {
+            return SendError(res, 400, "Product not found")
+        }
+        const FoundedCart = await Cart.findOne({ UserId: userId })
+        if (!FoundedCart) {
+            return SendError(res, 400, "Cart not found")
+        }
+        const index = FoundedCart.Items.findIndex(item => item.ProductId.toString() === productId.toString())
+
+        if (index > -1) {
+            FoundedCart.Items[index].Quantity--
+            FoundedCart.CartPrice -= FoundedProduct.Price
+            if (FoundedCart.Items[index].Quantity === 0) {
+                FoundedCart.Items.splice(index, 1)
+                await FoundedCart.save()
+                return SendError(res, 200, `${FoundedProduct.Name} removed from cart completely`)
+            }
+            await FoundedCart.save()
+            return SendSuccess(res, 200, `${FoundedProduct.Name} removed from cart`, { CurrentQuantity: FoundedCart.Items[index].Quantity })
+        }
+        return SendError(res, 400, "Product is not in your cart")
+
+    } catch (error) {
+        return SendError(res, 500, "Unknown error")
+
+    }
+}
+
+export const RemoveCart = async (req: AuthRequest, res: Response) => {
+    try {
+        let { id: userId } = req.User
+        const FoundedCart = await Cart.findOne({ UserId: userId })
+        if (!FoundedCart) {
+            return SendError(res, 400, "Cart not found")
+        }
+        FoundedCart.Items.length = 0
+        FoundedCart.CartPrice=0
+        await FoundedCart.save()
+        return SendSuccess(res, 200, `Cart removed`, { FoundedCart })
+
+} catch (error) {
+    return SendError(res, 500, "Unknown error")
+
+}
 }
--- BackEnd/src/cart/cart.router.ts
+++ BackEnd/src/cart/cart.router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { Authenticate } from "../middlewares/authentication";
 import { authorizeRoles } from "../middlewares/authorization";
-import { AddToCart, GetCart } from "./cart.controller";
+import { AddToCart, GetCart, RemoveFromCart, RemoveCart } from './cart.controller';
 
 
 export const CartRouter = Router()
 
 CartRouter.post("/:id", Authenticate, authorizeRoles("User"), AddToCart)
-CartRouter.get("/",Authenticate,authorizeRoles("User"),GetCart)
+CartRouter.get("/", Authenticate, authorizeRoles("User"), GetCart)
+CartRouter.put("/:id", Authenticate, authorizeRoles("User"), RemoveFromCart)
+CartRouter.delete("/",Authenticate,authorizeRoles("User"),RemoveCart)
