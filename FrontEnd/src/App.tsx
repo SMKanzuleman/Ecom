341b006c2a1edb96ac302a3196fe2f5ca33f3b67
SMKanzuleman <syedkanzuleman.official@gmail.com>
2026-07-28 09:07:19 +0500

    Frontend setup

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,8 @@
+export const App = () => {
+  return (
+    <div>
+      <h1 className="bg-blue-300 text-pink-700 min-h-screen text-9xl font-extrabold flex justify-center items-center">App Screen</h1>
+      <p>I am making full stack application</p>
+    </div>
+  )
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
-{
-    "Name": ""
-}
+# Table of Contents
+
+- [Introduction](#introduction)
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Configuration](#configuration)
+- [Contributing](#contributing)
+- [License](#license)
