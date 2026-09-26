5b8d90a5c3daee213dd4b668af0622f1df3a2f35
SMKanzuleman <syedkanzuleman.official@gmail.com>
2026-09-26 21:42:42 +0500

    fix: update CORS origin to allow requests from production URL

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(morgan("dev"))
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:2024",
+    origin: "https://ecom-epvl.onrender.com",
     credentials: true
 }))
 app.use("/auth/", AuthRouter)
